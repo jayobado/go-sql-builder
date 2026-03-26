853da5b33aced10bd748f6c76e5fb2bbe00cde4c
Jeremy Obado <jayobado@gmail.com>
2026-03-26 05:47:11 +0300

    Update main.go
    Signed-off-by: Jeremy Obado <jayobado@gmail.com>

--- examples/user/main.go
+++ examples/user/main.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq" // or mysql / mssql driver
-	"github.com/jayobado/sql-builder/sqb"
+	"github.com/jayobado/go-sql-builder/sqb"
 )
 
 type User struct {
